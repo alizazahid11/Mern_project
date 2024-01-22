ca2b77d73542dbddd6cbcafdbf0a0f3806ea81c1
aliza zahid <alizayzah11@gmail.com>
2024-01-22 12:22:27 -0800

    adding routing in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css';
-
+import Nav from './components/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<div className="App">
+  <BrowserRouter>
+
+<Nav/>
+{/* adding route to project (means content inside the links) */}
+<Routes>
+  <Route path="/" element={<h1>product component</h1>}></Route>
+  <Route path="/add" element={<h1>Add product component</h1>}></Route>
+  <Route path="/update" element={<h1>update component</h1>}></Route>
+  <Route path="/logout" element={<h1>logout component</h1>}></Route>
+  <Route path="/profile" element={<h1>profile component</h1>}></Route>
+</Routes>
+
+</BrowserRouter>
+</div>
+</>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Nav=()=>{
+    return(
+       <>
+       <ul className="nav-ul">
+           {/* adding links */}
+           <li><Link to ="/">Products</Link></li>
+           <li><Link to ="/add">add Products</Link></li>
+           <li><Link to ="/update">update Products</Link></li>
+           <li><Link to ="/logout">logout</Link></li>
+           <li><Link to ="/profile">Profile</Link></li>
+       </ul>
+       </> 
+    )
+}
+export default Nav;
