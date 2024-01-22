476714319708a4bef7fafa586f88842204fe5a62
aliza zahid <alizayzah11@gmail.com>
2024-01-22 15:54:35 -0800

    if user signup so show logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+
 const Nav=()=>{
-    return(
-       <>
-       <ul className="nav-ul">
-           {/* adding links */}
-           <li><Link to ="/">Products</Link></li>
-           <li><Link to ="/add">add Products</Link></li>
-           <li><Link to ="/update">update Products</Link></li>
-           <li><Link to ="/logout">logout</Link></li>
-           <li><Link to ="/profile">Profile</Link></li>
-           <li><Link to ="/signup">Signup</Link></li>
-       </ul>
-       </> 
-    )
+    const auth=localStorage.getItem('user');
+    return (
+        <>
+          <div>
+            <ul className="nav-ul">
+              {/* adding links */}
+              <li><Link to="/">Products</Link></li>
+              <li><Link to="/add">Add Products</Link></li>
+              <li><Link to="/update">Update Products</Link></li>
+      
+              <li><Link to="/profile">Profile</Link></li>
+              {auth ? 
+                <li><Link to="/logout">Logout</Link></li>
+              
+                :<li><Link to="/signup">Signup</Link></li>
+              }
+            </ul>
+          </div>
+        </>
+      );
+      
+    
 }
 export default Nav;
