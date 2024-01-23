068fd034f94d6c8d9929b3a47bffc43a75157a14
aliza zahid <alizayzah11@gmail.com>
2024-01-23 08:11:01 -0800

    integrate delete api

--- src/components/productlist.js
+++ src/components/productlist.js
@@ -10,7 +10,18 @@ const ProductList=()=>{
         setProducts(result);
     }
 
-    console.warn(products);
+const deleteproduct=async(id)=>{
+    console.warn(id);
+    let result= await fetch(`http://localhost:8000/product/${id}`,{
+        method:"Delete"
+    });
+    result=await result.json();
+    if(result){
+        getProduct();
+    }
+    
+}
+
     return(
         <>
        <div className="product-list">
@@ -20,14 +31,16 @@ const ProductList=()=>{
         <li>Name</li>
         <li>Price</li>
         <li>Category</li>
+        <li>Operation</li>
   
     </ul>
     {
-        products.map((item,index)=><ul key={item}>
+        products.map((item,index)=><ul key={item._id}>
         <li>{index+1}</li>
         <li>{item.name}</li>
         <li>{item.price}</li>
         <li>{item.category}</li>
+        <li><button onClick={()=>deleteproduct(item._id)}>Delete</button></li>
   
     </ul>)
     }
