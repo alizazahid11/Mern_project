3670c130456c65f0e390097aaca89979407d6774
aliza zahid <alizayzah11@gmail.com>
2024-01-23 03:57:55 -0800

    hiding all components b4 login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,21 +11,26 @@ const Nav=()=>{
     return (
         <>
           <div>
+            {
+
+            auth?
             <ul className="nav-ul">
               {/* adding links */}
               <li><Link to="/">Products</Link></li>
               <li><Link to="/add">Add Products</Link></li>
               <li><Link to="/update">Update Products</Link></li>
-      
-              <li><Link to="/profile">Profile</Link></li>
-             {
-                    auth? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-                    :<><li><Link to="/signup">Signup</Link></li>
+             <li><Link to="/profile">Profile</Link></li>
+             <li><Link onClick={logout} to="/signup">Logout</Link></li>
+             </ul>
+:<ul className="nav-ul nav-right">
+             
+                  
+                    <li><Link to="/signup">Signup</Link></li>
                       <li><Link to="/login">Login</Link></li>
-
-                    </>
+                      </ul>
+                
                   }
-            </ul>
+           
           </div>
         </>
       );
