10f35901acefb7fcc3fe271c142dc4509b352ee3
aliza zahid <alizayzah11@gmail.com>
2024-01-23 06:51:02 -0800

    adding validation to add product

--- src/components/Addproduct.js
+++ src/components/Addproduct.js
@@ -6,7 +6,14 @@ const Addproduct=()=>{
     const[price,setPrice]=useState("");
     const[category,setCategory]=useState("");
     const[company,setCompany]=useState("");
+    const[error,setError]=useState(false)
+
+
     const addProduct=async()=>{
+        if(!name||!price||!company||!category){
+            setError(true);
+            return false;
+        }
         console.warn(name,price,category,company);
         const userId=JSON.parse(localStorage.getItem('user'))._id;
         console.warn(userId);
@@ -28,9 +35,13 @@ const Addproduct=()=>{
         <div className='product'>
             <h1>Addproduct</h1>
             <input type="text" placeholder="enter product name" className='inputBox'value={name} onChange={(e)=>setName(e.target.value)}></input>
+           {error && !name && <span className="invalid-input">Enter valid name</span>}
             <input type="text" placeholder="enter product price" className='inputBox'value={price} onChange={(e)=>setPrice(e.target.value)}></input>
+            {error && !price && <span className="invalid-input">Enter valid price</span>}
             <input type="text" placeholder="enter product category"className='inputBox'value={category} onChange={(e)=>setCategory(e.target.value)}></input>
+            {error && !category && <span className="invalid-input">Enter category</span>}
             <input type="text" placeholder="enter product comapny"className='inputBox' value={company} onChange={(e)=>setCompany(e.target.value)}></input>
+            {error && !company && <span className="invalid-input">Enter valid comapny</span>}
             <button onClick={addProduct} className='appButton'>Add product</button>
         </div>
     )
